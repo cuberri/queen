93f464a7fd22364c85f758bc9044c26334b58e8b
cuberri <cuberri@gmail.com>
2015-05-07 16:09:48 +1200

    Revert "Merge branch 'gh-pages' of https://github.com/cuberri/queen into docker"
    This reverts commit 4ccb416f4891ab3c98cf4de7a5c52dfe2aa2a778, reversing
    changes made to b94c726aeaa7cdc9e8590121c4e0e0189800a8d9.

--- ping-client.js
+++
@@ -1,9 +0,0 @@
-// queenSocket is a global variable queen injects in 
-// to this context
-queenSocket.onMessage = function(number){
-	// Wait one second, then send the number + 1 back
-	// to the server-side script
-	setTimeout(function(){
-		queenSocket(number + 1);
-	}, 1000);
-};
